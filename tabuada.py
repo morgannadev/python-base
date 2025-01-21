dc6b1bb369c3264f4547a309fe6d8543bfb97378
morgannadev <morgcontato@gmail.com>
2025-01-21 14:08:02 -0300

    formatação da tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,7 +1,17 @@
+template_base = """
+---Tabuada do 2---
+
+     {bloco}
+
+##################
+"""
+
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do: ", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("------------------------")
+for n1 in numeros:
+    print("{:-^18}".format("Tabuada do " + str(n1)))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("\n" + "#" * 18)
