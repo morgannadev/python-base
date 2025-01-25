407be36817f799951dcd235272e044cb9a4b23c2
morgannadev <morgcontato@gmail.com>
2025-01-25 13:41:12 -0300

    adiciona exercicio com set e dict

---
+++ escola_v2_com_sets.py
@@ -0,0 +1,38 @@
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por sala que frequentam
+cada uma das atividades.
+"""
+__version__ = "0.1.1"
+
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["João", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+atividades = [
+    ("Inglês", aula_ingles),
+    ("Música", aula_musica),
+    ("Dança", aula_danca),
+]
+
+for nome_atividade, atividade in atividades:
+    print(f"Alunos da atividade {nome_atividade}\n")
+    print("-" * 50)
+    
+    #sala1 que tem interseção com a atividade - comparação O(1)
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2).intersection(atividade)
+
+    # comparação O(n)
+    # for aluno in atividade:
+    #     if aluno in sala1:
+    #         atividade_sala1.append(aluno)
+    #     elif aluno in sala2:
+    #         atividade_sala2.append(aluno)
+
+    print("Sala 1: ", atividade_sala1)
+    print("Sala 2: ", atividade_sala2)
+    print("#" * 50)
