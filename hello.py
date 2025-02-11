5f7e07e6ff1db6ecdb2c5093d00ec65c76e9538c
morgannadev <morgcontato@gmail.com>
2025-02-11 17:59:13 -0300

    nova versão hello.py

--- hello.py
+++ hello.py
@@ -8,25 +8,53 @@ Como usar:
 Tenha a variável LANG devidamente configurada. Ex:
     export LANG=pt_BR
 
+Ou informe através do CLI argument `--lang`
+Ou o usuário terá que digitar.
+
 Execução:
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Morganna Giovanelli"
 __licence__ = "Unlicense"
 
 import os
+import sys 
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+
+    key = key.lstrip("-").strip()
+    value = value.strip()
+
+    if key not in arguments:
+        print(f"Invalid option: `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
 
 msg = {
-    "en_US": "Hello World",
-    "pt_BR": "Olá Mundo",
-    "it_IT": "Ciao Mondo",
-    "es_SP": "Hola Mundo",
-    "fr_FR": "Bonjour le Monde",
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, le Monde!",
 }
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
